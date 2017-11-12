9de9b0db76be66ecdb00ccc3fa6768f74b5e61b1
Denis Borisov <webtrudnn@mail.ru>
2017-11-12 11:33:17 +0300

    Add react-select

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Article from './Article';
 import Accordion from '../decorators/Accordion';
 import Userform from './Userform';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class ArticleList extends Component {
 
@@ -11,11 +13,19 @@ class ArticleList extends Component {
         // from Accordion
         openItemId: PropTypes.string,
         toggleOpenItem: PropTypes.func
+    };
 
+    state = {
+        selection: null
     };
 
     render() {
         const {articles, openItemId, toggleOpenItem} = this.props;
+        const options = articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }));
+
         const articleElements = articles.map(article => {
             return <Article
                 key = {article.id}
@@ -28,12 +38,19 @@ class ArticleList extends Component {
         return (
             <section>
                 <Userform />
+                <Select options = {options}
+                        value = {this.state.selection}
+                        onChange = {this.changeSelection}
+                        multi = {true} />
                 <h1>Articles</h1>
                 {articleElements}
             </section>
         )
     }
 
+    changeSelection = (selection) => this.setState({selection})
 }
+
+
 
 export default Accordion(ArticleList);
